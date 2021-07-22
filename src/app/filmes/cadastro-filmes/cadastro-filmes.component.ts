784400879d883a8b3296a051873a907a60fba4bd
lucasjesusone <lucasjesusone@gmail.com>
2021-07-21 21:40:23 -0400

    Criando Formulário de Criação de Filme

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective
+     | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,13 +24,14 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  form: FormGroup;
+  submitted = false
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
 
-    this.options = this.fb.group({
+    this.form = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(2), Validators.maxLength]],
       urlPhoto: ['', Validators.required],
       dtLancamento: ['', Validators.required],
@@ -26,4 +43,21 @@ export class CadastroFilmesComponent implements OnInit {
 
   }
 
+
+  goBack() {
+    this.router.navigate(['/filmes'])
+  }
+
+  save(): void {
+    this.submitted = true
+    if(this.form.invalid) {
+      return
+    }
+
+    alert('Success' + JSON.stringify(this.form.value, null, 4));
+  }
+
+  resetForm(): void{
+    this.form.reset()
+  }
 }
