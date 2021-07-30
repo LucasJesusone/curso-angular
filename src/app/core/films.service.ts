34afd7c54f6df82189ea68e85a78d1f59d0ce5ce
lucasjesusone <lucasjesusone@gmail.com>
2021-07-30 02:06:16 -0400

    new services

--- src/app/core/films.service.ts
+++ src/app/core/films.service.ts
@@ -1,23 +1,23 @@
-import { Movie } from '../shared/models/Movie';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ConfigParamsService } from './config-params.service';
+import { ConfigParams } from './../shared/models/config-params';
+import { Movie } from "../shared/models/Movie";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
-const url = 'http://localhost:3000/filmes/'
+const url = "http://localhost:3000/filmes/";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FilmsService {
-
-
-  constructor(private httpCliente: HttpClient) { }
-
+  constructor(private httpCliente: HttpClient, private configService: ConfigParamsService) {}
 
   save(movie: Movie): Observable<Movie> {
-    return this.httpCliente.post<Movie>(url, movie)
+    return this.httpCliente.post<Movie>(url, movie);
   }
 
-  getAll(): Observable<Movie[]> {
-    return this.httpCliente.get<Movie[]>(url);
+  getAll(config: ConfigParams): Observable<Movie[]> {
+    const configParams = this.configService.configureParams(config)
+    return this.httpCliente.get<Movie[]>(url, { params: configParams });
   }
 }
