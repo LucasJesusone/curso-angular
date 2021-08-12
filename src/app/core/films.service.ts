6d8d4d5035b067149ab7cf7f3543a75d15d44dd9
lucasjesusone <lucasjesusone@gmail.com>
2021-08-11 23:54:08 -0400

    Changing url to environment base

--- src/app/core/films.service.ts
+++ src/app/core/films.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Movie } from './../shared/models/Movie';
 import { ConfigParamsService } from './config-params.service';
 import { ConfigParams } from './../shared/models/config-params';
@@ -5,39 +6,42 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-const url = "http://localhost:3000/filmes/";
+
 @Injectable({
   providedIn: "root",
 })
 export class FilmsService {
-  constructor(private httpCliente: HttpClient, private configService: ConfigParamsService) {}
+  private baseUrlService: string;
+  constructor(private httpCliente: HttpClient, private configService: ConfigParamsService) {
+    this.baseUrlService = environment.url
+  }
 
   save(movie: Movie): Observable<Movie> {
-    return this.httpCliente.post<Movie>(url, movie);
+    return this.httpCliente.post<Movie>(this.baseUrlService, movie);
   }
 
   getAll(config: ConfigParams): Observable<Movie[]> {
     const configParams = this.configService.configureParams(config)
-    return this.httpCliente.get<Movie[]>(url, { params: configParams });
+    return this.httpCliente.get<Movie[]>(this.baseUrlService, { params: configParams });
   }
 
   getAllWithoutFilter(): Observable<Movie[]> {
     // const configParams = this.configService.configureParams()
-    return this.httpCliente.get<Movie[]>(url);
+    return this.httpCliente.get<Movie[]>(this.baseUrlService);
   }
 
   view(id: number): Observable<Movie> {
-    return this.httpCliente.get<Movie>(url + id)
+    return this.httpCliente.get<Movie>(this.baseUrlService + id)
   }
 
 
   delete(id: number): Observable<void>{
-    return this.httpCliente.delete<void>(url + id)
+    return this.httpCliente.delete<void>(this.baseUrlService + id)
   }
 
 
   edit(movie: Movie): Observable<Movie> {
-    return this.httpCliente.put<Movie>(url + movie.id, movie)
+    return this.httpCliente.put<Movie>(this.baseUrlService + movie.id, movie)
   }
   
 }
